d27c369b6c72309de408ec5f55e426d451bc0e9b
Sam Ullman <samullman@gmail.com>
2023-01-22 13:22:20 -0600

    :wrench: initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import '@kahi-ui/framework/dist/kahi-ui.framework.min.css';
+	import '@kahi-ui/framework/dist/kahi-ui.theme.default.min.css';
+</script>
+
+<main>
+	<slot />
+</main>
+
+<style lang="scss">
+	main {
+		background: blue;
+	}
+</style>
